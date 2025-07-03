1b2565f549fe17ea22e6d9419917aa80c99c9a71
chizuru_jethro <uchechijethro1@gmail.com>
2025-07-03 08:42:33 +0100

    Did the same thing to the Packages

--- src/app/(root)/packages/[productName]/page.tsx
+++ src/app/(root)/packages/[productName]/page.tsx
@@ -2,9 +2,13 @@ import NavigationTab from "@/components/NavigationTab";
 import Package from "@/components/Package";
 import React from "react";
 
-const ProductName = async ({ params }: { params: { productName: string } }) => {
-  const { productName } = await params;
-  console.log(productName);
+const ProductName = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  const { slug } = await params;
+
   return (
     <>
       <NavigationTab
@@ -12,10 +16,10 @@ const ProductName = async ({ params }: { params: { productName: string } }) => {
           { title: "Home", href: "/" },
           { title: "Packages", href: "/packages" },
           { title: "Chairs", href: "/packages" },
-          { title: productName },
+          { title: slug, href: `/packages/${slug}` },
         ]}
       />
-      <Package product={productName} />
+      <Package product={slug} />
     </>
   );
 };
