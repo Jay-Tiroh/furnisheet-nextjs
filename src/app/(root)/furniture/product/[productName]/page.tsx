be564dc8432ad03cd6911ed497d0f41d8f3d9f24
chizuru_jethro <uchechijethro1@gmail.com>
2025-07-03 01:21:56 +0100

    Fixed another issue. Never following the philosophy of "if it ain't broke, don't fix it" again

--- src/app/(root)/furniture/product/[productName]/page.tsx
+++ src/app/(root)/furniture/product/[productName]/page.tsx
@@ -2,9 +2,15 @@ import Furniture from "@/components/Furniture";
 import NavigationTab from "@/components/NavigationTab";
 import React from "react";
 
-const ProductName = async ({ params }: { params: { productName: string } }) => {
-  const { productName } = await params;
-  console.log(productName);
+type Props = {
+  params: {
+    productName: string;
+  };
+};
+
+const ProductName = ({ params }: Props) => {
+  const { productName } = params;
+
   return (
     <>
       <NavigationTab
